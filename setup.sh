5e3811dfba9ec32dcaa4d56a3b65158549cdc764
BB9z <bb9z@myopera.com>
2013-05-07 15:18:25 +0800

    Add IDETextKeyBindingSet modify.

--- setup.sh
+++ setup.sh
@@ -11,4 +11,10 @@ mv -iv ~/Library/Developer/Xcode/Templates ~/Library/Developer/Xcode/Templates$R
 echo "\nCreat link to new directories:"
 ln -hfsv "$PWD/Templates" "$PWD/UserData" ~/Library/Developer/Xcode
 
+echo "\nBackup old IDETextKeyBindingSet:"
+sudo mv -iv  /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources/IDETextKeyBindingSet.plist IDETextKeyBindingSet/IDETextKeyBindingSet.plist
+
+echo "\nCreat IDETextKeyBindingSet link:"
+sudo ln -hfsv "$PWD/IDETextKeyBindingSet/IDETextKeyBindingSet.plist" "/Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources/IDETextKeyBindingSet.plist"
+
 echo "\nSetup end."
